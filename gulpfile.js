7a9f8bfad3429435cb94117c30f695d0479b0aab
1111hui <26634873@qq.com>
2016-11-03 19:17:29 +0800

    fix: index.d.ts file updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
-
-var gulp = require('gulp')
-var bump = require('gulp-bump')
+const f = require('util').format
+const fs = require('fs')
+const exec = require('child_process').exec
+const gulp = require('gulp')
+const bump = require('gulp-bump')
 
 // Basic usage:
 // Will patch the version
-gulp.task('bump', function () {
+gulp.task('bump', () => {
   gulp.src('./package.json')
     .pipe(bump())
     .pipe(gulp.dest('./'))
 })
 
+gulp.task('tag', () => {
+  const version = JSON.parse(fs.readFileSync('./package.json', 'utf8')).version
+  exec(
+    f(`git tag '%s' && git push --tags`, version),
+    err => console.log(err ? err : f('version "%s" published successfully.', version))
+  )
+})
--- index.d.ts
+++ index.d.ts
@@ -1,47 +1,59 @@
-// declare module "cssobj"{}
+// cssobj definition
 
-// main function
-export default function cssobj (obj: Object, options?: Options, data?: any): Result;
+declare namespace CssObj {
+    interface Options {
+      local?: boolean | LocalOption;
+      cssom?: cssomOption;
+      plugins?: any[];
+    }
 
-interface Options {
-  local?: boolean | LocalOption;
-  cssom?: cssomOption;
+    interface cssomOption {
+        frame?: any,
+        prefixes?: string[],
+        name?: string,
+        attrs?: Object,
+        noMedia?: boolean
+    }
+
+    interface LocalOption {
+      prefix?: string;
+      localNames?: Object;
+    }
+
+    interface HTMLStyleElement {
+      id?: string;
+      className?: string;
+      sheet?: Object;
+      styleSheet?: Object;
+      media?: string;
+    }
+
+    interface Node {
+      children: Object;
+      diff?: Object;
+      lastVal?: Object;
+      parentRule: any;
+      prevVal: any;
+      prop: Object;
+    }
+
+    interface Result {
+      update (data?: any): Result;
+      cssom: HTMLStyleElement;
+      data: Object;
+      nodes: Node[];
+      obj: Object;
+      options: Options;
+      ref: Object;
+      root: Node;
+    }
+
+    interface Static {
+        (obj: Object, options?: Options, data?: any): Result;
+    }
 }
 
-interface cssomOption {
-  name?: string;
-  attrs?: Object;
-}
-
-interface LocalOption {
-  prefix?: string;
-  localNames?: Object;
-}
-
-interface HTMLStyleElement {
-  id?: string;
-  className?: string;
-  sheet?: Object;
-  styleSheet?: Object;
-  media?: string;
-}
-
-interface Node {
-  children: Object;
-  diff?: Object;
-  lastVal?: Object;
-  parentRule: any;
-  prevVal: any;
-  prop: Object;
-}
-
-interface Result {
-  update (data?: any): Result;
-  cssom: HTMLStyleElement;
-  data: Object;
-  nodes: Node[];
-  obj: Object;
-  options: Options;
-  ref: Object;
-  root: Node;
+declare module 'cssobj' {
+    const cssobj: CssObj.Static
+    export = cssobj
 }
